5a6158b91e92fd2094ac1bb40b188cf40d188ae7
volkovich <Kirill.Volkovich@jCatalog.com>
2016-11-22 14:12:52 +0300

    Improved build.

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,5 @@
 // Here should be es2016 or CommonJS exports.
+module.exports = {
+  RichEditor: require('./components/RichEditor').default,
+  EmailRichEditor: require('./components/EmailRichEditor').default
+}
