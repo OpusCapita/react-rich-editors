cafc4df34308b173928fd1358300020292df528d
volkovich <Kirill.Volkovich@jCatalog.com>
2016-11-24 14:02:16 +0300

    Implemented 'replaceTextOfSelection' method.

--- src/client/components/RichEditor/lib/selection.js
+++ src/client/components/RichEditor/lib/selection.js
@@ -1,3 +1,9 @@
+import {
+  EditorState,
+  Modifier
+} from 'draft-js';
+import decorator from './decorator';
+
 export function getPlainTextOfSelection(editorState, selectionState) {
   let start = selectionState.getStartOffset();
   let end = selectionState.getEndOffset();
@@ -7,11 +13,8 @@ export function getPlainTextOfSelection(editorState, selectionState) {
   return selectedText;
 }
 
-export function replaceTextOfSelection(editorState, selectionState) {
-  let start = selectionState.getStartOffset();
-  let end = selectionState.getEndOffset();
-  let currentContent = editorState.getCurrentContent()
-  let currentBlock = currentContent.getBlockForKey(selectionState.getStartKey())
-  let selectedText = currentBlock.getText().slice(start, end);
-  return selectedText;
+export function replaceTextOfSelection(editorState, selection, replaceBy) {
+  let prevContentState = editorState.getCurrentContent();
+  let nextContentState = Modifier.replaceText(prevContentState, selection, replaceBy);
+  let nextEditorState = EditorState.createWidthContent(nextContentState, decorator);
 }
