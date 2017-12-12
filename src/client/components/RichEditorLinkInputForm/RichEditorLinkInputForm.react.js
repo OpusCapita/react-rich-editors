94930823f0fe1cc554fb75fe3bc5dde1208e1f11
baliunov <baliunov@scand.com>
2017-12-12 17:15:09 +0300

    Migrate to React 16

--- src/client/components/RichEditorLinkInputForm/RichEditorLinkInputForm.react.js
+++ src/client/components/RichEditorLinkInputForm/RichEditorLinkInputForm.react.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import s from './RichEditorLinkInputForm.module.less';
 import ShortcutContainer from '../ShortcutContainer';
 import FakeInputAutocomplete from '@opuscapita/react-autocompletes/lib/FakeInputAutocomplete';
