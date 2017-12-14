8f6430294415288260f498b118de302fd4798329
Alexey Sergeev <sab@scand.com>
2017-12-14 16:54:25 +0300

    Updating CircleCi build process

--- scripts/gh-pages/build.sh
+++ scripts/gh-pages/build.sh
@@ -3,5 +3,4 @@
 rm -rf .gh-pages-tmp &&
 node node_modules/@opuscapita/react-showroom-server/src/bin/showroom-scan.js src &&
 node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js &&
-cp -R lib/* .gh-pages-tmp &&
 cp -R src/client/demo/index.html .gh-pages-tmp
