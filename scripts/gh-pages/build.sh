71189e57a96d233ca615df88ed3745198b9b0d2d
Dmitriy Sanko <dmitriy.sanko@opuscapita.com>
2021-06-10 16:20:32 +0300

    Update gh-pages build

--- scripts/gh-pages/build.sh
+++ scripts/gh-pages/build.sh
@@ -2,5 +2,5 @@
 
 rm -rf .gh-pages-tmp &&
 node node_modules/@opuscapita/react-showroom-server/src/bin/showroom-scan.js src &&
-node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js &&
+node node_modules/webpack/bin/webpack.js --config ./webpack.development.config.js &&
 cp -R src/client/demo/index.html .gh-pages-tmp
